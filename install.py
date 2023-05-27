d275acc1cf0d05eec202038dd2a4c855feafd0d5
Pranitha-Gorenka <131530404+Pranitha-Gorenka@users.noreply.github.com>
2023-05-27 22:50:03 +0200

    Add files via upload

--- install.py
+++ install.py
@@ -1,1 +1,330 @@
+from dotenv import load_dotenv
+import os
+import sys
+import subprocess
+import re
+import socket
+import datetime
+import requests
+
+# Get the current date and time
+current_time = datetime.datetime.now()
+
+# Format the date and time
+formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+
+# Extract command-line arguments
+openrc_file = sys.argv[1]
+tag = sys.argv[2]
+ssh_key = sys.argv[3]
+
+# Load environment variables from the OpenRC file
+load_dotenv(openrc_file)
+
+# Access the environment variables
+username = os.getenv("OS_USERNAME")
+password = os.getenv("OS_PASSWORD")
+auth_url = os.getenv("OS_AUTH_URL")
+# ... and so on
+print(f"{formatted_time}: Starting deployment of {tag} using {openrc_file} for credentials. ")
+# Define server names
+server_names = ["node1", "node2", "node3", "proxy2", "proxy1", "bastion" ]
+
+network_list = "openstack network list"
+network_name = subprocess.run(network_list, shell=True, capture_output=True, text=True).stdout
+
+print(f"{formatted_time}: checking for nework in the OpenStack project..")
+# Check if network exists
+if f"{tag}_network" in network_name:
+    network_name = f"{tag}_network"
+    print(f"{formatted_time}: Network found")
+else:
+    # Create network
+    create_network = f"openstack network create {tag}_network"
+    subprocess.run(create_network, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    print(f"{formatted_time}: network {tag}_network does not exists! creating a network..")
+
+    # Create subnet
+    create_subnet = f"openstack subnet create {tag}_network-subnet --network {tag}_network --subnet-range 10.0.1.0/27"
+    subprocess.run(create_subnet, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    print(f"{formatted_time}: creating a {tag}_network-subnet for {tag}_network..")
+
+    # Create router
+    create_router = f"openstack router create {tag}_network-router"
+    subprocess.run(create_router, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    print(f"{formatted_time}: creating a router..")
+
+    # Set external gateway for the router
+    create_ext = f"openstack router set {tag}_network-router --external-gateway ext-net"
+    subprocess.run(create_ext, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    print(f"{formatted_time}: creating a external gateway for router..")
+
+    # Connect subnet to router
+    connect = f"openstack router add subnet {tag}_network-router {tag}_network-subnet"
+    subprocess.run(connect, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    print(f"{formatted_time}: adding router to subnet..")
+
+#create a key
+key_pair = "openstack keypair list"
+key_pair_name = subprocess.run(key_pair, shell=True, capture_output=True, text=True).stdout
+
+print(f"{formatted_time}: checking for {tag}_key in the OpenStack project..")
+        
+if f"{tag}_key" in key_pair_name:
+    key_pair_name = f"{tag}_key"
+    print(f"{formatted_time}: keypair found")
+else:
+    create_key=f"openstack keypair create --public-key {ssh_key} {tag}_key"
+    subprocess.run(create_key, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    print(f"{formatted_time}: creating a {tag}_key..")
+
+#fetchin localhost ip address
+def get_ip_address():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    return ip_address
+
+current_ip = get_ip_address()
+
+
+# Create a new security group
+security_group = "openstack security group list"
+security_group_name = subprocess.run(security_group, shell=True, capture_output=True, text=True).stdout
+
+print(f"{formatted_time}: checking for {tag}_security-group in the OpenStack project..")
+        
+if f"{tag}_security-group" in security_group_name:
+    security_group_name = f"{tag}_security-group"
+    print(f"{formatted_time}: {tag}_security-group found")
+else:
+    create_security_group = f"openstack security group create {tag}_security-group"
+    subprocess.run(create_security_group, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    # Add the required rules to the new security group
+    create_ssh_rule = f"openstack security group rule create --protocol tcp --dst-port 22 {tag}_security-group"
+    create_http_rule = f"openstack security group rule create --protocol tcp --dst-port 80 {tag}_security-group"
+    create_https_rule = f"openstack security group rule create --protocol tcp --dst-port 443 {tag}_security-group"
+    create_flask_rule = f"openstack security group rule create --protocol tcp --dst-port 5000 {tag}_security-group" 
+    create_snmp_rule = f"openstack security group rule create --protocol udp --dst-port 6000 {tag}_security-group"
+    create_snmpd_rule = f"openstack security group rule create --protocol udp --dst-port 161 {tag}_security-group"
+    create_icmp_rule = f"openstack security group rule create --protocol icmp --dst-port 22 {tag}_security-group"
+
+    subprocess.run(create_ssh_rule, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(create_http_rule, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(create_https_rule, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(create_flask_rule, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(create_snmp_rule, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(create_snmpd_rule, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(create_icmp_rule, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+    print(f"{formatted_time}: Created a  {tag}_security-group and added the required rules.")
+
+# Create servers
+
+node_ips = []
+for server_name in server_names:
+    check_server = f"openstack server show {server_name}"
+    result = subprocess.run(check_server, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if result.returncode == 0:
+        # Server with the same name already exists
+        print(f"{formatted_time}: Server '{server_name}' already exists.")
+        continue  # Skip creating the server
+    else:
+        create_server = f"openstack server create --flavor b.1c1gb --network {tag}_network --key-name {tag}_key --boot-from-volume 8 --image e6cbd963-8c28-4551-a837-e3b85da5d7a1 --security-group {tag}_security-group --wait {server_name}"
+        subprocess.run(create_server, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        print(f"{formatted_time}:Creating server {server_name}...")
+        
+    # Fetch the IP address of the server
+    fetch_ip_command = f"openstack server show -f value -c addresses {server_name}"
+    ip_address_output = subprocess.run(fetch_ip_command, shell=True, capture_output=True, text=True).stdout.strip()
+
+    # Extract the IP address from the output using regex
+    ip_address = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', ip_address_output)[0]
+
+    # Append the IP address to the node_ips list
+    node_ips.append(ip_address)
+       
+# Get floating IPs
+floating_list = "openstack floating ip list"
+floating_list_output = subprocess.run(floating_list, shell=True, capture_output=True, text=True).stdout
+
+# Extract the floating IP addresses
+floating_ips = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', floating_list_output)
+fixed_ips = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/\d+', floating_list_output)
+
+# Check if two floating IPs exist
+if len(floating_ips) >= 2:
+    print(f"{formatted_time}: Found 2 floating IPs.")
+    floating_ip_bastion = floating_ips[0]
+    floating_ip_proxy1 = floating_ips[1]
+    
+    # Assign floating IPs to servers
+    assign_floating_ip_bastion = f"openstack server add floating ip bastion {floating_ip_bastion}"
+    assign_floating_ip_proxy1 = f"openstack server add floating ip proxy1 {floating_ip_proxy1}"
+    
+    subprocess.run(assign_floating_ip_bastion, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(assign_floating_ip_proxy1, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    
+    print(f"{formatted_time}: Assigned floating IPs to bastion and proxy1.")
+else:
+    print(f"{formatted_time}: Did not find 2 floating IPs. Creating new ones...")
+    
+    # Create floating IP for Bastion
+    create_floating_ip_bastion = "openstack floating ip create ext-net"
+    subprocess.run(create_floating_ip_bastion, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    
+    # Create floating IP for HAproxy
+    create_floating_ip_proxy1 = "openstack floating ip create ext-net"
+    subprocess.run(create_floating_ip_proxy1, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+    # Get the newly created floating IPs
+    floating_list_output = subprocess.run(floating_list, shell=True, capture_output=True, text=True).stdout
+    floating_ips = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', floating_list_output)
+
+    if len(floating_ips) >= 2:
+        floating_ip_bastion = floating_ips[0]
+        floating_ip_proxy1 = floating_ips[1]
+        
+        # Assign floating IPs to servers
+        assign_floating_ip_bastion = f"openstack server add floating ip bastion {floating_ip_bastion}"
+        assign_floating_ip_proxy1 = f"openstack server add floating ip proxy1 {floating_ip_proxy1}"
+    
+        subprocess.run(assign_floating_ip_bastion, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.run(assign_floating_ip_proxy1, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    
+        print(f"{formatted_time}: Assigned floating IPs to bastion and proxy1.")        
+        
+    else:
+        print(f"{formatted_time}: Failed to fetch or create the floating IP addresses.")
+        exit()
+
+print(f"{formatted_time}: all nodes are done..")
+print(floating_ip_bastion)
+print(floating_ip_proxy1)
+
+
+# Build SSH config file
+ssh_config_content = f"""Host bastion
+  HostName {floating_ip_bastion}
+  User Ubuntu
+  IdentityFile ~/.ssh/authorized_keys
+  UserKnownHostsFile=~/dev/null
+  StrictHostKeyChecking no
+  PasswordAuthentication no
+
+Host proxy1
+  HostName {floating_ip_proxy1}
+  User Ubuntu
+  IdentityFile ~/.ssh/authorized_keys
+  UserKnownHostsFile=~/dev/null
+  StrictHostKeyChecking no
+  PasswordAuthentication no
+  ProxyJump bastion
+
+Host proxy2
+  HostName {node_ips[0]}
+  User Ubuntu
+  IdentityFile ~/.ssh/authorized_keys
+  UserKnownHostsFile=~/dev/null
+  StrictHostKeyChecking no
+  PasswordAuthentication no
+  ProxyJump bastion
+
+Host node1
+  HostName {node_ips[1]}
+  User Ubuntu
+  IdentityFile ~/.ssh/authorized_keys
+  UserKnownHostsFile=~/dev/null
+  StrictHostKeyChecking no
+  PasswordAuthentication no
+  ProxyJump bastion
+
+Host node2
+  HostName {node_ips[2]}
+  User Ubuntu
+  IdentityFile ~/.ssh/authorized_keys
+  UserKnownHostsFile=~/dev/null
+  StrictHostKeyChecking no
+  PasswordAuthentication no
+  ProxyJump bastion
+
+Host node3
+  HostName {node_ips[3]}
+  User Ubuntu
+  IdentityFile ~/.ssh/authorized_keys
+  UserKnownHostsFile=~/dev/null
+  StrictHostKeyChecking no
+  PasswordAuthentication no 
+  ProxyJump bastion  
+"""
+
+ssh_config_file_path = f"{tag}_SSHconfig"  # Path to the SSH config file
+with open(ssh_config_file_path, "w") as ssh_config_file:
+    ssh_config_file.write(ssh_config_content)
+
+print(f"{formatted_time}: Building base SSH config file, saved to {tag}_SSHconfig (current folder)")
+
+# Build hosts file
+hosts_file_path = "hosts"
+with open(hosts_file_path, "w") as hosts_file:
+    hosts_file.write("[Bastion]\n")
+    hosts_file.write("bastion \n")
+
+    hosts_file.write("\n[HAproxy]\n")
+    hosts_file.write("proxy1 \n")
+    hosts_file.write("proxy2 \n")
+    hosts_file.write("\n")
+
+    hosts_file.write("[nodes]\n")
+    hosts_file.write(f"node1\n")
+    hosts_file.write(f"node2\n")
+    hosts_file.write(f"node3\n")
+    hosts_file.write("\n[all:vars]\nansible_user=Ubuntu \n")
+    hosts_file.write("\n##REMOVED sshkey entry, handled by external SSH config file ##  \n")
+    
+
+# Run Ansible playbook for deplyoment
+print(f"{formatted_time}: Running playbook")
+ansible_playbook = f"ansible-playbook -i hosts --ssh-common-args='-F./{tag}_SSHconfig' site1.yaml"
+playbook_execution = subprocess.run(ansible_playbook, shell=True,stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+if playbook_execution.returncode == 0:
+    print(f"{formatted_time}: OK")
+else:
+    ansible_playbook1 = f"ansible-playbook -i hosts site1.yaml"
+    playbook_execution1 = subprocess.run(ansible_playbook1, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    if playbook_execution1.returncode == 0:
+        print(f"{formatted_time}: OK")
+    else:
+       ansible_playbook2 = f"ansible-playbook site1.yaml"
+       playbook_execution2 = subprocess.run(ansible_playbook2, shell=True,stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+       if playbook_execution2.returncode == 0:
+           print(f"{formatted_time}: OK")
+       else:
+           print(f"{formatted_time}: Error executing playbook.") 
+           
+# Get floating IPs
+floating_list = "openstack server show proxy1 -c addresses -f value"
+floating_list_output = subprocess.run(floating_list, shell=True, capture_output=True, text=True).stdout
+
+# Extract the floating IP addresses
+floating_ips = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', floating_list_output)
+fixed_ips = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/\d+', floating_list_output)
+
+print("Validates Operation")
+for floating_ip in floating_ips:
+    print("")
+
+# Define the URL to browse
+url = "http://" + floating_ip
+
+result = subprocess.run("openstack server list -c Name -f value", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+existing_nodes = re.findall(r'^node\d+', result.stdout, re.MULTILINE)
+
+for i, node in enumerate(existing_nodes, start=1):
+    response = requests.get(url, proxies={"http": floating_ip, "https": floating_ip})
+
+    # Print the page content
+    print(f"Response {i}: {response.content.decode()}")
+
+print("OK")
 
---
+++ operate.py
@@ -0,0 +1,203 @@
+from dotenv import load_dotenv
+import os
+import sys
+import subprocess
+import re
+import time
+import datetime
+import requests
+
+# Get the current date and time
+current_time = datetime.datetime.now()
+
+# Format the date and time
+formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+
+# Extract command-line arguments
+openrc_file = sys.argv[1]
+tag = sys.argv[2]
+ssh_key = sys.argv[3]
+
+# Load environment variables from the OpenRC file
+load_dotenv(openrc_file)
+
+# Access the environment variables
+username = os.getenv("OS_USERNAME")
+password = os.getenv("OS_PASSWORD")
+auth_url = os.getenv("OS_AUTH_URL")
+# ... and so on
+
+# Define server names
+server_names = ["node1", "node2", "node3"]
+
+while True:
+    # Read server.conf to get the required number of nodes
+    with open('server.conf', 'r') as file:
+        config_lines = file.readlines()
+
+    # Extract the number of nodes required from server.conf
+    num_nodes = None
+    for line in config_lines:
+        if "num_nodes =" in line:
+            num_nodes_match = re.search(r'num_nodes = (\d+)', line)
+            if num_nodes_match:
+                num_nodes = int(num_nodes_match.group(1))
+            break
+
+    # Calculate the number of new nodes to create
+    if num_nodes is None:
+        print(f"{formatted_time}: Unable to find the required number of nodes in server.conf.")
+        sys.exit(1)
+
+    print(f"{formatted_time}: Reading server.conf, we need {num_nodes} nodes.")
+    
+    result = subprocess.run("openstack server list -c Name -f value", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    existing_nodes = re.findall(r'^node\d+', result.stdout, re.MULTILINE)
+    print(f"{formatted_time}: Checking solution, we have: {len(existing_nodes)} nodes.")
+    
+    if len(existing_nodes) == num_nodes:
+        # Update the number of nodes in server.conf
+        config_lines = [line.replace(f"num_nodes = {num_nodes}", f"num_nodes = {num_nodes + 1}") for line in config_lines]
+        with open('server.conf', 'w') as file:
+            file.writelines(config_lines)
+        time.sleep(30)
+        
+    elif len(existing_nodes) < num_nodes:
+        num_new_nodes = num_nodes - len(existing_nodes)
+        print(f"{formatted_time}: Detecting lostnode : {num_new_nodes} .")
+        
+        # Create new nodes
+        for i in range(num_new_nodes):
+            new_node_number = len(existing_nodes) + i + 1
+            new_node_name = f"node{new_node_number}"
+            
+            # Check if the new node name already exists
+            while new_node_name in existing_nodes:
+                new_node_number += 1
+                new_node_name = f"node{new_node_number}"
+
+            create_server = f"openstack server create --flavor b.1c1gb --network {tag}_network --key-name {tag}_key --boot-from-volume 8 --image e6cbd963-8c28-4551-a837-e3b85da5d7a1 --security-group {tag}_security-group --wait {new_node_name}"
+            subprocess.run(create_server, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            existing_nodes.append(new_node_name)
+            print(f"{formatted_time}: created : {tag}_{new_node_name}")
+            
+        # Fetch the IP addresses of the new nodes
+        node_ips = []
+        for new_node_name in existing_nodes:
+            fetch_ip_command = f"openstack server show -f value -c addresses {new_node_name}"
+            ip_address_output = subprocess.run(fetch_ip_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            if ip_address_output.returncode == 0:
+                ip_addresses = re.findall(r'\d+\.\d+\.\d+\.\d+', ip_address_output.stdout)
+                if ip_addresses:
+                    node_ips.append(ip_addresses[0])
+
+        # Build hosts file
+        with open('hosts', 'r') as file:
+            host_lines = file.readlines()
+
+        updated_host_lines = []
+        for line in host_lines:
+            updated_host_lines.append(line.strip())
+
+        # Find the index of the [nodes] group in the hosts file
+        nodes_index = -1
+        for i, line in enumerate(updated_host_lines):
+           if line.strip() == '[nodes]':
+               nodes_index = i
+               break
+
+        if nodes_index != -1:
+            # Insert the new nodes after the [nodes] group
+            updated_host_lines.insert(nodes_index + 1, f'{new_node_name}')
+
+        # Write the updated hosts file
+        with open('hosts', 'w') as file:
+            file.write("\n".join(updated_host_lines))
+            
+        # Update the SSH config file
+        with open(f'{tag}_SSHconfig', 'r') as file:
+            ssh_lines = file.readlines()
+
+        updated_ssh_lines = []
+        for line in ssh_lines:
+            updated_ssh_lines.append(line)
+
+        for new_node_name, node_ip in zip(existing_nodes[-num_new_nodes:], node_ips[-num_new_nodes:]):
+            ip_address_line = f"  HostName {node_ip}"  # Indent the HostName line correctly
+            host_entry_found = False
+
+            for i, line in enumerate(updated_ssh_lines):
+                if f"Host {new_node_name}" in line:
+                    if "HostName" in updated_ssh_lines[i+1]:
+                        updated_ssh_lines[i+1] = f"  HostName {node_ip}\n"
+                    else:
+                        updated_ssh_lines.insert(i+1, f"  HostName {node_ip}\n")
+                        host_entry_found = True
+                        break
+
+            if not host_entry_found:
+                updated_ssh_lines.append(f"\nHost {new_node_name}\n  HostName {node_ip}\n  User Ubuntu\n  IdentityFile ~/.ssh/authorized_keys\n  UserKnownHostsFile=/dev/null\n  StrictHostKeyChecking no\n  PasswordAuthentication no\n  ProxyJump bastion\n")
+
+        with open(f'{tag}_SSHconfig', 'w') as file:
+            file.writelines(updated_ssh_lines)
+
+        print(f"{formatted_time}: Updated {tag}_SSHconfig") 
+        
+        # Run Ansible playbook for deployment
+        print(f"{formatted_time}: Running playbook")
+        ansible_playbook = f"ansible-playbook -i hosts --ssh-common-args='-F./{tag}_SSHconfig' site.yaml"
+
+        playbook = subprocess.run(ansible_playbook, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+        if playbook.returncode == 0:
+            print("")
+        else:
+            print(f"{formatted_time}: Error executing playbook.")
+
+        # Run Ansible Test environment playbook
+        ansible_playbook = f"ansible-playbook -i hosts --ssh-common-args='-F./SSHconfig' site1.yaml"
+
+        playbook_execution = subprocess.run(ansible_playbook, shell=True,stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+        if playbook_execution.returncode == 0:
+            print(f"{formatted_time}: OK")
+        else:
+            ansible_playbook1 = f"ansible-playbook -i hosts site1.yaml"
+            playbook_execution1 = subprocess.run(ansible_playbook1, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            if playbook_execution1.returncode == 0:
+                print(f"{formatted_time}: OK")
+            else:
+                ansible_playbook2 = f"ansible-playbook site1.yaml"
+                playbook_execution2 = subprocess.run(ansible_playbook2, shell=True,stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            if playbook_execution2.returncode == 0:
+                print(f"{formatted_time}: OK")
+            else:
+                print(f"{formatted_time}: Error executing playbook.") 
+        time.sleep(30)
+       
+        # Get floating IPs
+        floating_list = "openstack server show proxy1 -c addresses -f value"
+        floating_list_output = subprocess.run(floating_list, shell=True, capture_output=True, text=True).stdout
+
+        # Extract the floating IP addresses
+        floating_ips = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', floating_list_output)
+        fixed_ips = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/\d+', floating_list_output)
+
+        print("Validates Operation")
+        for floating_ip in floating_ips:
+            print("")
+
+        # Define the URL to browse
+        url = "http://" + floating_ip
+
+        result = subprocess.run("openstack server list -c Name -f value ", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        existing_nodes_all = re.findall(r'^node\d+', result.stdout, re.MULTILINE)
+
+        for i, node in enumerate(existing_nodes_all, start=1):
+            response = requests.get(url, proxies={"http": floating_ip, "https": floating_ip})
+
+            # Print the page content
+            print(f"Response {i}: {response.content.decode()}")
+
+        print("OK")
+
